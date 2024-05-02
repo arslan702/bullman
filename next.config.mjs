9f1b1f25bda420aa2695a82e0f52aca262bee91d
mycomputer <muhammadarslanjabbar@gmail.com>
2024-05-02 10:14:45 +0500

    img-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["localhost", "bullman-api.onrender.com", "bullman.netlify.app"],
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+            pathname: '**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'bullman-api.onrender.com',
+            pathname: '**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'bullman.netlify.app',
+            pathname: '**',
+          }
+        ]
+        // remotePatterns: ["localhost", "bullman-api.onrender.com", "bullman.netlify.app"],
       },
 };
 
